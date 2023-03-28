75ed69719c9e0206352f5893ad9f844f694b8742
NeeRaj-2401 <niraj.yadav@slrtce.in>
2023-03-28 15:49:46 +0530

    play/pause icons, UI improvements, hover etc

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Loader from './Loader';
+import { FaPlay, FaPause } from 'react-icons/fa';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,30 +23,29 @@ function App() {
             if (station.language !== "hindi" || station.country !== "India" || url.startsWith("http://") || url.endsWith(".m3u8")) {
               return map; // skip stations if not hindi/india or having hrl http:// or .m3u8
             }
-            
+
             if (!map[url]) {
               map[url] = station; // add the station to the map if it hasn't been seen before
             }
             return map;
           }, {}))
-
-          // const filteredStations = results.filter( station => station.language === "hindi" || station.language === "Hindi")
           console.log(filteredStations)
           setStations(filteredStations);
           setIsLoading(false);
         });
-      } catch (error) {
-        console.log({ error });
-        setIsLoading(false)
-      }
-    }, []);
-  
+    } catch (error) {
+      console.log({ error });
+      setIsLoading(false)
+    }
+  }, []);
+
 
   const audioRef = useRef(null);
   const [currentStation, setCurrentStation] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false)
 
   function handleStationClick(station) {
+    setIsLoading(true);
     audioRef.current.src = station.url;
     audioRef.current.play();
     setCurrentStation(station);
@@ -56,19 +56,26 @@ function App() {
     audioRef.current.pause();
     setIsPlaying(false);
   };
+  const handlePlay = () => {
+    audioRef.current.play();
+    setIsPlaying(true);
+  };
 
   return (
     <div className="App min-h-screen bg-gray-900 text-gray-100 py-4">
-      <h1 className="text-3xl font-bold text-gray-100 pb-4">Trending Hindi Stations</h1>
-      <div className="container mx-auto px-4 md:px-0">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <h1 className="text-2xl font-bold text-gray-100 pb-4">Trending Hindi Stations</h1>
+      <div className="container mx-auto px-4 md:px-0 mt-4 sm:mt-10 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 md:gap-10">
 
-          {<audio ref={audioRef} />}
+          <audio ref={audioRef} onPlay={() => setIsLoading(false)} />
+
+          {isLoading && <Loader />}
+
           {stations.map((station) => (
             <div
               key={station.stationuuid}
               className={`bg-gray-800 rounded-md flex items-center py-2 px-4 shadow-md border border-gray-700 
-      ${currentStation?.url === station.url ? 'bg-green-500' : ''}`}
+              ${currentStation?.url === station.url ? 'bg-green-500' : 'hover:bg-gray-600 hover:border-gray-100`'}`}
 
             >
               <div className="flex items-center justify-center h-12 w-12 rounded-full bg-gray-700 mr-4 cursor-pointer" onClick={() => handleStationClick(station)}>
@@ -88,19 +95,18 @@ function App() {
               </div>
 
               <h2 className={`text-sm font-semibold title cursor-pointer ${currentStation?.url === station.url ? 'text-xs text-gray-900 ' : ''}`} onClick={() => handleStationClick(station)}>{station.name}</h2>
-              {currentStation?.url === station.url && isPlaying && (
+              {currentStation?.url === station.url && (isPlaying ? (
                 <button onClick={handlePause} className="ml-auto mr-4">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6 text-gray-100">
-                    <path fill="currentColor" d="M6 4h4v16H6V4zm8 0h4v16h-4V4z" />
-                  </svg>
+                  <FaPause className="h-6 w-6 hover:text-green-200" style={{ strokeWidth: 1 }} />
                 </button>
-              )}
-
+              ) : (
+                <button onClick={handlePlay} className="ml-auto mr-4">
+                  <FaPlay className="h-6 w-6 hover:text-green-200" style={{ strokeWidth: 1 }} />
+                </button>
+              ))}
 
             </div>
           ))}
-
-
         </div>
       </div>
       {isLoading && <Loader />}
--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 function Loader() {
     return (
         <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-800 bg-opacity-50">
-            <div className="flex items-center justify-center bg-white p-8 rounded-md shadow-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="animate-spin h-8 w-8 text-gray-500 mr-2">
-                    <circle cx="12" cy="12" r="10" fill="none" stroke="currentColor" strokeWidth="4" strokeDasharray="15 60" />
-                </svg>
-
-                <span className="text-gray-800 text-base font-semibold">Loading Stations...</span>
+            <div className="flex items-center justify-center bg-white p-8 rounded-md shadow-lg bg-gray-800">
+            <ScaleLoader color="#36d7b7" />
+                {/* <span className="text-gray-800 text-base font-semibold">Stations...</span> */}
             </div>
         </div>
     );
